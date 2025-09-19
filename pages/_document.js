39d969b3a307f8193b61723f22d562face181015
rahildevops <rahil.document@gmail.com>
2025-09-19 23:37:51 +0530

    added favicon

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,8 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=swap" rel="stylesheet" />
+        {/* Favicon */}
+        <link rel="icon" href="/images/dataGuradFavicon.png" />
       </Head>
       <body>
         <Main />
